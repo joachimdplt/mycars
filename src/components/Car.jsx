70e82f41bc0debb5f8fd96744e3de24b6df1a083
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-08-24 12:36:07 +0200

    Ajout du composant Cars.jsx

---
+++ src/components/Car.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Car = () => {
+    return (
+        <div>
+            <p>Make : </p>
+            <p>Color :</p>
+        </div>
+    )
+}
+
+export default Car;
--- src/components/Mycars.jsx
+++ src/components/Mycars.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Car from "./Car";
 
 class Mycars extends React.Component {
     render() {
         return (
             <div>
                 <h1>Hello React</h1>
+                <Car />
             </div>
         )
     }
