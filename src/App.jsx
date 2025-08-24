49ec6614c18d3dc68f0e6458a6b8662182416761
joakmannn <165683883+joakmannn@users.noreply.github.com>
2025-08-24 12:24:48 +0200

    Create class component and import in parent element

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import Mycars from './components/Mycars';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Mycars />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Mycars.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Mycars extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React</h1>
+            </div>
+        )
+    }
+}
+
+export default Mycars;
